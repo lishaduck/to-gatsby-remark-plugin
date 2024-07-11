ad24cf72f7e9283d3f9439c408e0c1babb193b53
Eli <88557639+lishaduck@users.noreply.github.com>
2024-07-10 20:52:10 -0500

    chore: add types

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+// @ts-check
 import vfile from "to-vfile";
 
-function toGatsbyRemarkPlugin(remarkPlugin) {
-  return function ({ markdownAST, markdownNode }, options) {
-    const file = vfile(markdownNode.fileAbsolutePath);
+/**
+ * @import { Plugin, Processor, Settings, Transformer } from "unified";
+ */
 
-    return remarkPlugin(options)(markdownAST, file);
+/**
+ * @template T
+ * @typedef {(this: Processor<Settings>, { markdownAST, markdownNode }: {markdownAST: any; markdownNode: any; }, options: T) => any} GatsbyRemarkPlugin
+ */
+
+/**
+ * @template T
+ * @param {Plugin} remarkPlugin
+ * @returns {GatsbyRemarkPlugin<T>}
+ */
+function toGatsbyRemarkPlugin(remarkPlugin) {
+  return function ({ markdownAST, markdownNode }, /** @type {T} */ options) {
+    const file = vfile(markdownNode.fileAbsolutePath);
+    /** @type {Transformer | void} */
+    const builtPlugin = remarkPlugin.call(this, options);
+
+    if (builtPlugin instanceof Function) {
+      return builtPlugin(markdownAST, file);
+    }
   };
 }
 
